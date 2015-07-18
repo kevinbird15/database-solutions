02f3ae1b93388a452281352076d040acf4935e35
kevinbird15 <kevinbird15@gmail.com>
2015-07-18 17:20:21 -0500

    adding active to clicked tab

--- public/javascripts/frontpage.js
+++ public/javascripts/frontpage.js
@@ -2,6 +2,9 @@ var main = function(){
   $('.menu').click(function(){
     $(this).fadeOut();
   });
+  $('nav li').click(function(){
+    $(this).addClass('active')
+  });
 };
 
 $(document).ready(main);
