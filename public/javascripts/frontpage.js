5791420fa6ff2ba6d51149fe436f6ffeceafac79
kevinbird15 <kevinbird15@gmail.com>
2015-07-18 17:21:27 -0500

    fixniig

--- public/javascripts/frontpage.js
+++ public/javascripts/frontpage.js
@@ -2,7 +2,7 @@ var main = function(){
   $('.menu').click(function(){
     $(this).fadeOut();
   });
-  $('nav li').click(function(){
+  $('.nav li').click(function(){
     $(this).addClass('active')
   });
 };
