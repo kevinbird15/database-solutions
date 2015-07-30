39a7ebdcc41fa09786d8b6678bb1b46faefce50b
kevinbird15 <kevinbird15@gmail.com>
2015-07-29 23:16:04 -0500

    got a skeleton built that I can tolerate

--- app/controllers/seed_controller.rb
+++ app/controllers/seed_controller.rb
@@ -1,21 +1,21 @@
-class SeedController < ApplicationController
+class SeedController < SeedlayoutController
   def index
-
+    @title="Seed Home"
   end
 
   def product
-
+    @title="Seed Product"
   end
 
   def pallet
-
+    @title="Seed Pallet"
   end
 
   def transaction
-
+    @title="Seed Transaction"
   end
 
   def treatment
-
+    @title="Seed Treatment"
   end
 end
